952e6ce180d631aa18e9b899e01cd1f4b9dd9f86
Willington Vega <wvega@wvega.com>
2012-09-10 20:34:21 -0500

    First Commit.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Natalia Martínez &mdash; Comunicadora Social</title>
+        <link type="text/css" rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css" />
+        <link type="text/css" rel="stylesheet" href="resources/bootstrap/css/bootstrap-responsive.min.css" />
+        <link type="text/css" rel="stylesheet" href="resources/css/style.css" />
+        <link href='http://fonts.googleapis.com/css?family=Life+Savers|Bilbo+Swash+Caps|Alegreya+SC|Junge|Homenaje' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <div id="page" class="container-fluid">
+            <div id="content" class="row-fluid">
+                <div id="resume" class="span12">
+                    <header>
+                        <hgroud>
+                            <h1>Natalia Martínez</h1>
+                            <h2>Comunicadora Social</h2>
+                            <address>
+                                <a href='m&#97;ilto&#58;nata%6D%61r%7&#52;inez&#103;&#97;%40&#103;mai%6&#67;%2E%63&#111;%&#54;D'>nata&#109;ar&#116;inez&#103;a&#64;&#103;ma&#105;l&#46;com</a>
+                            </address>
+                        </hgroup>
+                    </header>
+                    <figure><img src="resources/css/images/natalia.jpg" alt="Natalia Martínez"/></figure>
+                    <footer>
+                        <ul id="contact" class="clearfix">
+                            <li class="facebook">
+                                <a href="https://www.facebook.com/natalia.martinez.31945243" title="Agrégame en Facebook"><img src="resources/css/images/fb.png" /></a>
+                            </li>
+                            <li class="twitter">
+                                <a href="https://twitter.com/natyca122" title="Sígueme en Twitter"><img src="resources/css/images/twitter.png" /></a>
+                            </li>
+                            <li class="google-plus">
+                                <a href="https://plus.google.com/u/0/106280125480864881123" title="Agrágame a tus círculos en Google+"><img src="resources/css/images/gp.png" /></a>
+                            </li>
+                        </ul>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
